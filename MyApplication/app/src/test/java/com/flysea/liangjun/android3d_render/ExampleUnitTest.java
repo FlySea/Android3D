72ef557196e307b667abf56f566ca2f52fc413ea
Flysea <419121044@qq.com>
2016-01-15 15:01:52 +0800

    新建android工程 androidSDK4.4.4

---
+++ MyApplication/app/src/test/java/com/flysea/liangjun/android3d_render/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.flysea.liangjun.android3d_render;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+  @Test
+  public void addition_isCorrect() throws Exception {
+    assertEquals(4, 2 + 2);
+  }
+}
